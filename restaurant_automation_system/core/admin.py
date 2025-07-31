2b669fd341ff3dabb0f2c19fce0689769e14eff1
fay <wahomeimani@gmail.com>
2025-07-31 12:24:16 +0300

    models

--- restaurant_automation_system/core/admin.py
+++ restaurant_automation_system/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (
+    MenuItem, Order, OrderDetail, Ingredient,
+    ItemIngredient, Inventory, PurchaseOrder,
+    Invoice, Cheque
+)
 
-# Register your models here.
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
+admin.site.register(Ingredient)
+admin.site.register(ItemIngredient)
+admin.site.register(Inventory)
+admin.site.register(PurchaseOrder)
+admin.site.register(Invoice)
+admin.site.register(Cheque)
