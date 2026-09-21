7e3680d453bc14cca4ce8c924286373e711f3072
wahome <144784453+22koki@users.noreply.github.com>
2026-09-21 15:08:13 +0300

    fix authentication permission decorator import

--- restaurant_automation_system/core/views.py
+++ restaurant_automation_system/core/views.py
@@ -3,7 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.db import transaction
 from django.contrib.auth import authenticate
