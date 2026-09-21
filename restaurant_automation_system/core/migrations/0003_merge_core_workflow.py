d4cc75afa44d751eb35740fd332a8959604d919e
wahome <144784453+22koki@users.noreply.github.com>
2026-09-21 11:57:35 +0300

    merge parallel core migration branches

---
+++ restaurant_automation_system/core/migrations/0003_merge_core_workflow.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0002_cashbalance_cashregister_salesreport_and_more'),
+        ('core', '0002_restaurant_workflow'),
+    ]
+
+    operations = []
